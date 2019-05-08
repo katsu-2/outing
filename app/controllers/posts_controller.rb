f0d55e4aa07b722b5bb508f60584094ecf7f3e8d
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-08 12:34:45 +0900

    feat: hamlの導入

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
   end
 
   def edit
@@ -18,5 +21,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :answer)
   end
 end
