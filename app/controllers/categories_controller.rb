a15bb077c6b29a293f78501d32aff2c390c08a68
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-08 15:20:25 +0900

    feat: csvでカテゴリーを追加

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
+    @posts = Post.where(category_id: @category).includes(:user).includes(:category)
   end
 end
