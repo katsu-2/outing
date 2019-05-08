4a96ab21e349b10336ec878ac0d6c24ffe9e94e5
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-08 15:12:49 +0900

    feat: categoryリソースの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_08_053949) do
+ActiveRecord::Schema.define(version: 2019_05_08_061156) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment", null: false
