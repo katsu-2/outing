b9eea67de9bb0dfa9d3939d0f6a9fc047ab0345d
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-08 14:10:44 +0900

    feat: folderの中身作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = current_user.posts.recent.order('created_at desc')
+    @folders = Folder.where(user_id: current_user.id)
   end
 end
