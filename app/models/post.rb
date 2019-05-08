2f4f1da63e3cf9e1584b2a85ce06fa31a3382cea
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-08 14:42:37 +0900

    feat: アソシエーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :folder_posts, dependent: :destroy
   has_many :folders, through: :folder_posts
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
 
   scope :recent, -> { order('created_at desc') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :folders
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+
 end
