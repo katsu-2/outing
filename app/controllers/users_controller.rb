94968ac5727cbbe029f83d86aec789e9c245fd4d
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-08 17:58:30 +0900

    fix: flashの調整

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = current_user.posts.recent.page(params[:page]).per(6).order('created_at desc').includes(:category)
-    @folders = Folder.where(user_id: current_user.id)
+    @folders = Folder.where(user_id: current_user.id).includes(:user)
     @likes = Like.where(user_id: current_user.id)
   end
 end
