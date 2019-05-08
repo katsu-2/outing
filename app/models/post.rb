6c46ff6af5bae9e1f8eb82b3ebf773bd8a11cf54
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-08 14:05:42 +0900

    feat: アソシエーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :folder_posts, dependent: :destroy
+  has_many :folders, through: :folder_posts
 
   scope :recent, -> { order('created_at desc') }
 end
