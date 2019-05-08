a3443231509b800c1d59bbc7163eab3507805ecc
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-08 14:03:36 +0900

    feat: routingの追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
 
   root 'posts#index'
   resources :users, only: [:index, :show]
-  # resources :folders do
-  #   resources :posts
-  # end
+  resources :folders do
+    resources :posts
+  end
 
   resources :posts do
     resources :comments, only: [:create, :destroy]
