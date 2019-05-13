2e34099a90137a7fbef21fad9ba5e589864aac11
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-13 16:07:42 +0900

    feat: folderモデルのspec追加

--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -1,11 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Folder, type: :model do
-  # describe "バリデーションテスト" do
-  #   it "名前が無ければ無効" do
-  #     folder = create(:folder, name: nil)
-  #     folder.valid?
-  #     expect(folder.errors[:name]).to include("が入力されていません。")
-  #   end
-  # end
+  before do
+    @user = build(:user)
+  end
+
+  describe "バリデーションテスト" do
+    it "タイトルがあれば有効" do
+      folder = build(:folder, user: @user)
+      expect(folder).to be_valid
+    end
+
+    it "タイトルが無ければ無効" do
+      folder = build(:folder, name: nil, user: @user)
+      folder.valid?
+      expect(folder.errors[:name]).to include("が入力されていません。")
+    end
+
+    it "タイトルが20字以上で無効" do
+      folder = build(:folder, name: "a" * 21, user: @user)
+      folder.valid?
+      expect(folder.errors[:name]).to include("は20文字以下に設定して下さい。")
+    end
+  end
 end
