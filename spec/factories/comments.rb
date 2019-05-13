4cbdfa2ff13357932d408259d459a42cc13dead2
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-13 15:59:24 +0900

    feat: commentモデルのspec追加

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    
+    comment {'こんにちは'}
+    user
+    post
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = build(:user)
+    @post = build(:post)
+  end
+
+  describe "バリデーションテスト" do
+    it "ユーザー、問題、コメントがあれば有効" do
+      comment = build(:comment, user: @user, post: @post)
+      expect(comment).to be_valid
+    end
+
+    it "コメントがないと無効" do
+      comment = build(:comment, comment: nil, user: @user, post: @post)
+      comment.valid?
+      expect(comment.errors[:comment]).to include("が入力されていません。")
+    end
+
+    it "コメントが1000字以上は無効" do
+      comment = build(:comment, comment: "a" * 1001, user: @user, post: @post)
+      comment.valid?
+      expect(comment.errors[:comment]).to include("は1000文字以下に設定して下さい。")
+    end
+  end
 end
