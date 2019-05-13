689fb78be339f9d655d095b725f88de8a6bc5c57
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-13 15:36:31 +0900

    feat: comment_specの追加

--- spec/factories/folders.rb
+++ spec/factories/folders.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :folder do
-    
+    name {'folder'}
+    user
   end
 end
--- spec/models/folder_spec.rb
+++ spec/models/folder_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Folder, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # describe "バリデーションテスト" do
+  #   it "名前が無ければ無効" do
+  #     folder = create(:folder, name: nil)
+  #     folder.valid?
+  #     expect(folder.errors[:name]).to include("が入力されていません。")
+  #   end
+  # end
 end
