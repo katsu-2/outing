babb760af4a19802ff77e4a65d6a87b3efb7cc4f
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-15 16:33:09 +0900

    feat: categoryコントローラのspec

---
+++ spec/controllers/categories_controller_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe CategoriesController, type: :controller do
+  describe "GET #index" do
+    before do
+      @user = create(:user)
+      @category = create(:category)
+    end
+
+    context "登録済みユーザー" do
+      before do
+        sign_in @user
+        get :index
+      end
+
+      it "正常にレスポンス返す" do
+        expect(response).to be_successful
+      end
+
+      it "200レスポンス返す" do
+        expect(response).to have_http_status 200
+      end
+
+      it "indexテンプレの表示" do
+        expect(response).to render_template :index
+      end
+
+    end
+
+    context "未登録ユーザー" do
+      before do
+        get :index
+      end
+
+      it "正常にレスポンス返す" do
+        expect(response).to be_successful
+      end
+
+      it "200レスポンス返す" do
+        expect(response).to have_http_status 200
+      end
+
+      it "indexテンプレの表示" do
+        expect(response).to render_template :index
+      end
+    end
+  end
+
+  describe "GET #show" do
+    context "登録済みユーザー" do
+
+    end
+
+    context "未登録ユーザー" do
+
+    end
+  end
+end
