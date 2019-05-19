58e0d705efedc18e48a1b46c52a9a70755c769c1
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-19 14:18:22 +0900

    rspec: 検索機能のテスト

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -30,4 +30,39 @@ RSpec.describe Post, type: :model do
       expect(post.errors[:answer]).to include("が入力されていません。")
     end
   end
+
+  describe "検索結果のテスト" do
+    before do
+      @post1 = create(:post,
+        title: "first title",
+        content: "aaa"
+      )
+
+      @post2 = create(:post,
+        title: "second title",
+        content: "aaa"
+      )
+
+      @post3 = create(:post,
+        title: "third title",
+        content: "content first"
+      )
+    end
+
+    context "一致するデータが見つかるとき" do
+      it "タイトル,問題どちらかが一致したデータを返す" do
+        expect(Post.search("first")).to include(@post1,@post3)
+      end
+    end
+
+    context "一致するデータが見つからないとき" do
+      it "当てはまる文字列がないと空のコレクションを返す" do
+        expect(Post.search("test")).to be_empty
+      end
+
+      it "空検索するとpostを全表示する" do
+        expect(Post.search(" ")).to include(@post1,@post2,@post3)
+      end
+    end
+  end
 end
