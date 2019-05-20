1467fbfebd876761277b55768ca31430cfcd4a7e
大平勝也 <k8t2y9.cat@gmail.com>
2019-05-20 11:30:41 +0900

    rspec: createアクションのテスト

---
+++ spec/controllers/likes_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe LikesController, type: :controller do
+  before do
+    @user = create(:user)
+    @post = create(:post, user: @user)
+    @like = create(:like, user_id: @user.id, post_id: @post.id)
+  end
+
+  describe "#create" do
+    context "登録済みユーザー" do
+      before do
+        sign_in @user
+      end
+
+      it "正常なレスポンスを返す" do
+        post :create, format: :json, params: { post_id: @post.id, id: @like.id }
+        expect(response.content_type).to eq "text/html"
+      end
+
+      it "いいねをする" do
+        expect{
+          post :create, format: :json, params: {
+            post_id: @post.id, id: @like.id
+          }
+        }.to change{Like.count}.by(1)
+      end
+    end
+
+    context "未登録ユーザー" do
+      before do
+        post :create, format: :json, params: { post_id: @post.id, id: @like.id }
+      end
+
+      it "レスポンスの失敗" do
+        expect(response).to_not be_success
+      end
+
+      it "いいねできない" do
+        expect{
+          post :create, format: :json, params: {
+            post_id: @post.id, id: @like.id
+          }
+        }.to change{Like.count}.by(0)
+      end
+    end
+  end
+end
